35541742f099e08a0e811c07c6b3dfd445abf27b
Sierra Wallace <sskwallace@gmail.com>
2020-10-11 00:44:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+  attr_accessor :side_1, :side_2, :side_3, :all_sides
+  
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+    @all_sides = []
+end
+  def kind 
+     if @side_1 == @side_2 && @side_2 == @side_3
+      return :equilateral
+    elsif @side_1 == @side_2 && @side_1 + @side_2 > @side_3 || @side_1 == @side_3 && @side_1 + @side_3 > @side_2 || @side_2 == @side_3 && @side_2 + @side_3 > @side_1
+      return :isosceles
+    elsif @side_1 != @side_2 && @side_2 != @side_3 && @side_1 + @side_2 > @side_3
+      return :scalene
+    else 
+        raise TriangleError
+      end
+    end
+
+  class TriangleError < StandardError
+    puts "Not a triangle."
+  end
 end
